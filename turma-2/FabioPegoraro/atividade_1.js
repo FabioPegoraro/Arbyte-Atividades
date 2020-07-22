fd787f22472bcdd8edf7cd2e13e0cff3d6081be0
FabioPegoraro <66393999+FabioPegoraro@users.noreply.github.com>
2020-07-22 18:56:59 -0300

    Lista 10.1

---
+++ turma-2/FabioPegoraro/atividade_1.js
@@ -0,0 +1,13 @@
+//Crie uma classe que represente um usuário de um app como uber ou 99. 
+//O Usuário deve ter alguns atributos como nome, email, endereço primário, formas de pagamento, etc.
+
+class Pessoa{
+    constructor(nome, email, enderecoPrimario, formaPagamento){
+        this.nome = nome
+        this.email = email
+        this.enderecoPrimario = enderecoPrimario
+        this.formaPagamento = formaPagamento
+    }
+}
+
+
